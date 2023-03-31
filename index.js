e3a8a9e3123c890e2085368b65f7dc153ed026d3
Bastien Leraut <bastien.leraut@free.fr>
2023-03-31 18:48:58 +0200

    Ajout d'une partir /character sur le site

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("./src/server");
 const { PORT } = require("./src/env")
 
+
 app.listen(PORT, () => {
     console.log(`listening port ${PORT}`)
 });
--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const ch = require('./routes/character.js');
+const path = require('path');
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', "ejs");
+
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
     console.log('oui');
     res.send('Hello World 1 5');
 });
+
+app.use('/character', ch);
 
 app.get('/test', function (req, res) {
     res.send('Page /test 1');
